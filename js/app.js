8e616d6c151f81bc6bdd8f49db3d01b7a837c156
juliachagas26 <julia.cto@outlook.com>
2020-07-01 20:06:34 -0300

    Adicionando input do usuário

--- js/app.js
+++ js/app.js
@@ -1,19 +1,35 @@
 document.getElementById("botaoJogar").onclick = () => {
+
+    const jogadorCara = element("jogadorCara").value;
+    const jogadorCoroa = element("jogadorCoroa").value;
+    let nomeVencedor = "";
+
     let valor = Math.random();
     let imagem = "";
+
+    if(jogadorCara == ""|| jogadorCoroa == ""){
+        return alert("Digite os nomes");
+    }
 
 
     if(valor > 0.5){
         console.log("Um");
         imagem = "coroa";
+        nomeVencedor = jogadorCoroa;
     }else{
         console.log("Zero");
         imagem = "cara";
+        nomeVencedor = jogadorCara;
     }
 
     let caminhoImagem = "./assets/" + imagem + ".png";
 
     document.getElementById("imagemMoeda").src = caminhoImagem;
 
-    console.log(valor);
+    alert(nomeVencedor);
 };
+
+function element(id){
+    return document.getElementById(id);
+}
+
